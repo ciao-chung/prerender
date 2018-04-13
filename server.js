15d1416626a39b4e19799be8e4be5f58d8fd4eae
Ciao Chung <ciao0958@gmail.com>
2018-04-14 02:22:13 +0800

    加入Memery Cache Plugin

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const prerender = require('./lib')
+const memeryCachePlugin = require('prerender-memory-cache')
 const server = prerender()
 
+server.use(memeryCachePlugin)
 server.use(prerender.sendPrerenderHeader())
 server.use(prerender.removeScriptTags())
 server.use(prerender.httpHeaders())
