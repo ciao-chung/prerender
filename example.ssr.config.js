999c5d45a850ec52f875ce36b8a0b76b46de5c20
Ciao Chung <ciao0958@gmail.com>
2018-06-11 12:59:33 +0800

    調整pm2設定檔

--- example.ssr.config.js
+++ example.ssr.config.js
@@ -3,8 +3,8 @@ module.exports = {
     name        : 'ssr',
     script      : '/path-to-server.js',
     env: {
-      'ALLOWED_DOMAINS': 'www.example.com,example.com',
-      'CACHE_TTL': '600',
+      ALLOWED_DOMAINS: 'www.example.com,example.com',
+      CACHE_TTL: '600',
     },
     // restart every monday
     cron_restart: '0 0 * * 1',
